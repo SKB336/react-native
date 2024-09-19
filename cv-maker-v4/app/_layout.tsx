fe760e707274353a185c8093981a921e680fca4e
ShakeebIVIS <shakeeb@intelliversal.com>
2024-09-19 10:16:37 +0500

    Fresh Project Setup

---
+++ cv-maker-v4/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className="text-blue-500">RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
