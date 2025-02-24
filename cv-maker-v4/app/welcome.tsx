c06f25ed53499f126a6e568bfdcef0bfa282f1dd
SKB336 <shakeebahmed336@gmail.com>
2025-02-24 19:55:10 +0500

    Feb-24

--- cv-maker-v4/app/welcome.tsx
+++ cv-maker-v4/app/welcome.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import { Svg, Circle } from 'react-native-svg';
+
+const { width, height } = Dimensions.get("window");
+
+// Make the diameter 1.5x the screen width
+const circleDiameter = 1.5 * width;
+const circleRadius = circleDiameter / 2;
+
+// 1/3 of the circle should be hidden
+const circleHiddenHeight = (1 / 3) * circleDiameter;
+
+// Calculate the top position dynamically
+const circleTop = -circleHiddenHeight;
 
 const WelcomeScreen = () => {
     return (
         <View className="flex-1 bg-white px-4 pt-6">
             <Svg
                 width="100%"
-                height="100%"
-                viewBox="0 0 100 50"
-                // style={{ position: 'absolute', top: -10, left: '-20%' }}
+                height="50%"
+                viewBox="0 0 100 100"
+                style={{ position: 'absolute', top: circleTop, left: '0%' }}
             >
                 <Circle cx="50" cy="50" r="50" fill="#1A5555" />
             </Svg>
@@ -17,4 +29,4 @@ const WelcomeScreen = () => {
     );
 };
 
-export default WelcomeScreen
+export default WelcomeScreen;
